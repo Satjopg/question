8fdcbad459189b138446aac53cf6d8297211c974
Satoru Murakami <Satoru@AirMacs.local>
2015-12-18 01:28:16 +0900

    add page

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('unq',{title:'unqate'});
+});
+
+router.get('/input', function(req, res, next) {
+ res.render('thk',{title:'thanks'});
 });
 
 module.exports = router;
